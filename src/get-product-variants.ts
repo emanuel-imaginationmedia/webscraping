42025d10b77886a8ab6dc484c615b271da3aff8a
Emanuel Massafera <emanuel@imaginationmedia.com>
2023-01-31 15:37:12 -0300

    fix: get products with only one variant

--- src/get-product-variants.ts
+++ src/get-product-variants.ts
@@ -19,11 +19,35 @@ export const getProductVariants = async (page: Page, url: string): Promise<Produ
     const variants: ProductVariant[] = [];
     const checkboxElementList = await page.$$('div[data-testid="color-checkbox"]');
 
-    for (let index = 0; index < checkboxElementList.length; index++) {
-        const elements = await page.$$('div[data-testid="color-checkbox"]');
-        await elements[index].click();
-        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    if (checkboxElementList && checkboxElementList.length) {
+        for (let index = 0; index < checkboxElementList.length; index++) {
+            const elements = await page.$$('div[data-testid="color-checkbox"]');
+            await elements[index].click();
+            await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
+            const product = await page.evaluate(() => {
+                const nameElement = document.querySelector('h1[data-testid="product-title-text"]');
+                const descriptionElement = document.querySelector('div[data-testid="product-collapsable-desc-content"] p');
+                const detailElementList = [...document.querySelectorAll('div[data-testid="product-collapsable-details-content"] li')];
+                const imageElementList = [...document.querySelectorAll('div[data-testid="product-image"] img')];
+                const productColorTitleElement = document.querySelector('div[data-testid="product-color-title"]');
+                const productSizeElementList = [...document.querySelectorAll('div[data-testid="size-checkbox"]')];
+                const singlePairElementList = [...document.querySelectorAll('div[data-testid="single-pair-button"]')];
+
+                return {
+                    name: nameElement ? nameElement.textContent : null,
+                    description: descriptionElement ? descriptionElement.textContent : null,
+                    details: detailElementList ? detailElementList.map((detail) => detail.textContent) : null,
+                    images: imageElementList ? imageElementList.map((image: any) => image.src) : null,
+                    productColorTitle: productColorTitleElement ? productColorTitleElement.textContent : null,
+                    sizes: productSizeElementList ? productSizeElementList.map((size) => size.textContent) : null,
+                    singlePair: singlePairElementList ? singlePairElementList.map((singlePair) => singlePair.textContent) : null,
+                };
+            });
+
+            variants.push(product);
+        }
+    } else {
         const product = await page.evaluate(() => {
             const nameElement = document.querySelector('h1[data-testid="product-title-text"]');
             const descriptionElement = document.querySelector('div[data-testid="product-collapsable-desc-content"] p');
